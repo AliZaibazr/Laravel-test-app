44521468e256221fbd60f833aac408c790fcd24d
Abdul majeed <abdul4majeed@gmail.com>
2021-02-19 11:00:01 +0500

    Create method functionality with Form Request

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Info;
+use Illuminate\Http\Request;
+use \App\Http\Requests\InfoRequest;
+class InfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        dd('$request');
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(InfoRequest $request)
+    {
+        // dd(($request->validated()));
+        if(Info::create($request->validated()))
+            return ['msg' => 'Record Created Successfully'];
+        else
+            return ['msg' => 'Something goes wrong'];
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Info  $info
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Info $info)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Info  $info
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Info $info)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Info  $info
+     * @return \Illuminate\Http\Response
+     */
+    public function update(InfoRequest $request, Info $info)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Info  $info
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Info $info)
+    {
+        //
+    }
+}
